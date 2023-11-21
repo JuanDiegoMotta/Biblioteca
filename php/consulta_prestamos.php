7477438002cc4c00d11d476fd7537cc6a8f68d87
MMJDMM <67686314+MMJDMM@users.noreply.github.com>
2023-11-21 18:30:38 +0100

    Modificaciones crea_tablas.php -> mejor control de errores y ejecución multiquery index.html -> añadidos nombres de los desarrolladores

---
+++ php/consulta_prestamos.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consulta libros</title>
+    <link rel="stylesheet" href="../css/consulta_libros.css">
+</head>
+
+<body>
+
+    <div class="titulo">
+        <h1>Libros que tenemos disponibles!</h1>
+    </div>
+
+    <?php
+    require_once 'controlBBDD/conecta.php';
+
+    try {
+        // Creamos la instancia de la clase BaseDeDatos
+        $bd = new BaseDeDatos();
+
+        // Intentamos conectarnos a la bbdd
+        if ($bd->conectar()) {
+            echo "<p>Conectado</p>";
+            $conexion = $bd->getConexion();
+            mysqli_select_db($conexion, "Biblioteca");
+
+            //   Consulta para seleccionar los libros con mas de 0 unidades disponibles
+            $sql = "SELECT * FROM libros WHERE n_disponibles > 0";
+            $resultado = mysqli_query($conexion, $sql);
+
+            if ($resultado) {
+                $rows = [];
+                // Se recuperan el resultado de la consulta
+                while ($row = mysqli_fetch_assoc($resultado)) {
+                    $rows[] = $row;
+                }
+
+                if (count($rows) > 0) {
+                    // Se muestra la tabla si hay resultados
+                    echo '<table border="1">';
+                    echo '<tr>';
+                    foreach ($rows[0] as $key => $value) {
+                        // Encabezado de la tabla
+                        echo "<th>$key</th>";
+                    }
+                    echo '</tr>';
+
+                    foreach ($rows as $row) {
+                        echo '<tr>';
+                        foreach ($row as $value) {
+                            // Datos de la tabla
+                            echo "<td>$value</td>";
+                        }
+                        echo '</tr>';
+                    }
+                    echo '</table>';
+                } else {
+                    echo "No se encontraron libros disponibles.";
+                }
+            } else {
+                echo "Error en la consulta: " . mysqli_error($conexion);
+            }
+
+            $bd->cerrar();
+        } else {
+            echo "<p>Error al conectar con la base de datos</p>";
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+    ?>
+
+    <div id="boton-home">
+        <a href="../index.html">
+            <button>HOME</button>
+        </a>
+    </div>
+
+</body>
+
+</html>
--- php/controlBBDD/crea_tablas.php
+++ php/controlBBDD/crea_tablas.php
@@ -1,24 +1,27 @@
 <?php
-    require_once 'conecta.php';
-    // Comprobamos si la conexión fue exitosa:
-    try{
-        $bd = new BaseDeDatos();
-        if($bd->conectar()){
-            echo "<p>Conectado</p>";
-            $conexion = $bd->getConexion();
-             // Verificamos si la base de datos ya existe
-            $query = "SHOW DATABASES LIKE 'Biblioteca'";
-            $result = mysqli_query($conexion, $query);
-            if(mysqli_num_rows($result) == 0){
-                echo "<p>No existe la BBDD Biblioteca, creándola...</p>";
-                // Creamos la BBDD
-                $sql = "CREATE DATABASE Biblioteca";
-                if(mysqli_query($conexion, $sql)){
-                    // Si se ha creado la BBDD exitosamente, procedo a crear las tablas
-                    echo "<p>Base de datos creada con éxito</p>";
-                    mysqli_select_db($conexion, "Biblioteca"); // Indica con qué BBDD se va a trabajar
-                    // Queries para crear las tablas:
-                    $sql1 = "CREATE TABLE libros (
+require_once 'conecta.php';
+
+try {
+    $bd = new BaseDeDatos();
+
+    if ($bd->conectar()) {
+        $conexion = $bd->getConexion();
+
+        $query = "SHOW DATABASES LIKE 'Biblioteca'";
+        $result = mysqli_query($conexion, $query);
+
+        if (mysqli_num_rows($result) == 0) {
+            // Crear la base de datos
+            $sqlCreateDB = "CREATE DATABASE Biblioteca";
+            if (mysqli_query($conexion, $sqlCreateDB)) {
+                echo "<p>Base de datos 'Biblioteca' creada con éxito.</p>";
+
+                // Cambiar al contexto de la nueva base de datos
+                mysqli_select_db($conexion, "Biblioteca");
+
+                // Crear las tablas
+                $sqlCreateTables = "
+                    CREATE TABLE libros (
                         id INT AUTO_INCREMENT PRIMARY KEY,
                         nombre VARCHAR(255),
                         autor VARCHAR(255),
@@ -27,66 +30,73 @@
                         sinopsis TEXT,
                         n_disponibles INT,
                         n_totales INT
-                    )";
-                    $sql2 = "CREATE TABLE lectores (
+                    );
+
+                    CREATE TABLE lectores (
                         id INT AUTO_INCREMENT PRIMARY KEY,
                         lector VARCHAR(255),
                         dni VARCHAR(9),
                         estado VARCHAR(255),
                         n_prestado INT
-                    )";
-                    $sql3 = "CREATE TABLE prestamos (
+                    );
+
+                    CREATE TABLE prestamos (
                         id_lector INT,
                         id_libro INT,
                         PRIMARY KEY (id_lector, id_libro),
                         FOREIGN KEY (id_lector) REFERENCES Lectores(id),
                         FOREIGN KEY (id_libro) REFERENCES Libros(id)
-                    )";
-                    // Queries para insertar 2 filas en cada tabla
-                    $sql4 = "INSERT INTO libros (nombre, autor, publicacion, isbn, sinopsis, n_disponibles, n_totales)
-                    VALUES 
-                        ('Cien años de soledad', 'Gabriel García Márquez', '1967-05-30', '978-84-204-6260-4', 'La novela narra la historia de la familia Buendía en el pueblo ficticio de Macondo.', 9, 10),
-                        ('To Kill a Mockingbird', 'Harper Lee', '1960-07-11', '0-06-112008-1', 'The novel explores the issues of racism and injustice in the American South.', 9, 10)";
-                        
-                    $sql5 = "INSERT INTO lectores (lector, dni, estado, n_prestado)
-                    VALUES 
-                        ('Juan Pérez', '12345678L', 'Activo', 1),
-                        ('María Gómez', '98765432G', 'Inactivo', 1)";
-                    $sql6 = "INSERT INTO prestamos (id_lector, id_libro)
-                    VALUES
-                        (1, 2),
-                        (2, 1)";
-                    // Ejecutamos las queries
-                    if(mysqli_query($conexion, $sql1)){
-                        echo "<p>Tabla libros creada correctamente</p>";
+                    );
+                ";
+
+                if (mysqli_multi_query($conexion, $sqlCreateTables)) {
+                    do {
+                        // Vaciamos los resultados de cada consulta
+                        mysqli_next_result($conexion);
+                    } while (mysqli_more_results($conexion));
+
+                    echo "<p>Tablas creadas correctamente.</p>";
+
+                    // Insertar datos
+                    $sqlInsertData = "
+                        INSERT INTO libros (nombre, autor, publicacion, isbn, sinopsis, n_disponibles, n_totales)
+                        VALUES 
+                            ('Cien años de soledad', 'Gabriel García Márquez', '1967-05-30', '978-84-204-6260-4', 'La novela narra la historia de la familia Buendía en el pueblo ficticio de Macondo.', 9, 10),
+                            ('To Kill a Mockingbird', 'Harper Lee', '1960-07-11', '0-06-112008-1', 'The novel explores the issues of racism and injustice in the American South.', 9, 10);
+
+                        INSERT INTO lectores (lector, dni, estado, n_prestado)
+                        VALUES 
+                            ('Juan Pérez', '12345678L', 'Activo', 1),
+                            ('María Gómez', '98765432G', 'Activo', 1);
+
+                        INSERT INTO prestamos (id_lector, id_libro)
+                        VALUES
+                            (1, 2),
+                            (2, 1);
+                    ";
+
+                    if (mysqli_multi_query($conexion, $sqlInsertData)) {
+                        do {
+                            // Vaciamos los resultados de cada consulta
+                            mysqli_next_result($conexion);
+                        } while (mysqli_more_results($conexion));
+
+                        echo "<p>Datos insertados correctamente.</p>";
+                    } else {
+                        throw new Exception("Error al insertar datos: " . mysqli_error($conexion));
                     }
-                    if(mysqli_query($conexion, $sql2)){
-                        echo "<p>Tabla lectores creada correctamente</p>";
-                    }
-                    if(mysqli_query($conexion, $sql3)){
-                        echo "<p>Tabla prestamos creada correctamente</p>";
-                    }
-                    if(mysqli_query($conexion, $sql4)){
-                        echo "<p>Valores añadidos correctamente a la tabla libros</p>";
-                    }
-                    if(mysqli_query($conexion, $sql5)){
-                        echo "<p>Valores añadidos correctamente a la tabla lectores</p>";
-                    }
-                    if(mysqli_query($conexion, $sql6)){
-                        echo "<p>Valores añadidos correctamente a la tabla prestamos</p>";
-                    }
-    
-                } else{
-                    echo "<p>Error al crear la base de datos</p>";
-                    mysqli_error($bd->getConexion()); // Muestra el código de error
+                } else {
+                    throw new Exception("Error al crear las tablas: " . mysqli_error($conexion));
                 }
-            } else{
-                echo "<p>La BBDD Biblioteca ya existe.</p>";
+            } else {
+                throw new Exception("Error al crear la base de datos: " . mysqli_error($conexion));
             }
-            $bd->cerrar();
+        } else {
+            echo "<p>La BBDD Biblioteca ya existe.</p>";
         }
-    } catch (Exception $e){
-        echo $e->getMessage();
-    }  
-        
-    ?>
+
+        $bd->cerrar();
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
